8846169c939c77dddb08e8ef2789dcdc9f0cd758
badassbilal <mbilalkhan500@gmail.com>
2023-09-07 22:45:19 -0500

    E BitManipulation/BitShift  ReverseBits.py (https://www.youtube.com/watch?v=UcoN6UjAI64) https://leetcode.com/problems/reverse-bits

--- Binary/ReverseBits.py
+++ Binary/ReverseBits.py
@@ -1,6 +1,23 @@
 
-def reverseBits(bit: int) -> int:
-    print(bit.)
+def reverseBits(bitnum: int) -> int:
+    # return int(f"b0{bin(bit)[:1:-1]}", 2)
+    rslt = 0
+    for i in range(32):
+        last_bit_of_bitnum = (bitnum >> i) & 1 # if bitnum = 101101 => 10110->1<- then last_bit = 1 on the right most.
+        '''
+        bitnum >> i move the i-th bit to the last. 
+        i = 0, the same number is present.
+        & 1 gives the last bit only (which we need in this case)
+        '''
+        rslt = rslt |last_bit_of_bitnum << 31-i
+        '''
+        now we OR with the result binary which is 32 zeroes.
+        if we have 1 in the last_bit, we move it 31-i to the left and then OR it so, it can be 1 as well.
+        if we have 0 in the last_bit, we ignore (altho operation is done).
+        '''
+    return (rslt)
+
+
 
 
 def main():
