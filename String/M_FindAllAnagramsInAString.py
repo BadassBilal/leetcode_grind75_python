793b2579e25598d93184d56284f3b90e7ebf813a
badassbilal <mbilalkhan500@gmail.com>
2023-09-10 18:35:53 -0500

    M FindAllAnagramsInAString.py https://leetcode.com/problems/find-all-anagrams-in-a-string/description/ (Still Working)

---
+++ String/M_FindAllAnagramsInAString.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findAnagrams(s: str, p: str) -> list[int]:
+        '''
+        # 55/65 Passed
+        rslt = []
+        ana = len(p)
+        sub = set(p)
+        for i, x in enumerate(range(len(s)+1-ana)):
+            substring = set(s[x:x + ana])
+            if substring == sub: rslt.append(i)
+        return rslt
+        '''
+
+
+
+
+def main():
+    print(Solution.findAnagrams(s = "cbaebabacd", p = "abc"))
+
+
+if __name__ == '__main__':
+    main()
